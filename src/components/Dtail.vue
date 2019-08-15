e0b7517e0456f6f493dc5b99a87dc7d2415445dc
qiwzee <warakronkiw@gmail.com>
2019-08-15 13:39:29 +0700

    เพิ่มหน้ารายละเอียด

---
+++ src/components/Dtail.vue
@@ -0,0 +1,68 @@
+<template>
+    <v-container class="grey lighten-5" sm>
+        <v-row> 
+        <v-row no-gutters >
+          <v-col cols="8" sm="6">
+            <v-card flat>
+            <v-img
+              src="../images/vr.png"
+              contain
+              height="400px"
+            > 
+            </v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="4" sm="6">
+            <v-card flat="" height="400"> 
+                <v-card-text>
+                <p class="display-1 text--primary">
+                    SEIKO 
+                </p>
+                 <p class="display-1 text--primary">
+                    PROSPEX 
+                </p>
+                <div class="text--primary">
+                    นาฬิกาข้อมือระบบ Solar<br>
+                    รุ่น SBDL035J
+                </div>
+                <p class="display-1 text--primary">
+                    ราคา 27,000 บาท
+                </p>
+                </v-card-text>
+                 <v-btn outlined width="200" height="50">ซื้อ</v-btn>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="12">
+              <v-card flat>
+                <v-card-text>
+                    <div>
+                        <p class="font-weight-black">ข้อมูลจำเพาะของสินค้า</p>
+                        <tr>
+                            
+                            <td><p class="font-weight-bold">ยี่ห้อ: </p></td>
+                            <td><p class="font-weight-regular">Seiko(ไซโก้)</p></td>
+                        </tr>
+                    </div>
+                    <div>
+                        <p class="font-weight-black">รายละเอียดสินค้า</p>
+                        <tr><p class="font-weight-regular">นาฬิกา SEIKO PROSPEX Solar Diver Diver 200m. รุ่น SBDL035J<br>
+                                Seiko Prospex Tuna Lowercase Limited Edition 3000 pcs.<br>
+                                Outer Case ผลิตจากยาง Polyurethane<br>
+                                กลไก V175 Solar Chronograph ชาร์จพลังงานจากแสงได้ทุกประเภท<br>
+                                จับเวลาที่ความละเอียด 1/5 วินาที ต่อเนื่องสูงสุด 60 นาที<br>
+                                ตัวเรือนและสาย Stainless steel black IP coating<br>
+                                ตัวเรือนขนาด 47 มิลลิเมตร<br>
+                                ประสิทธิภาพการกันน้ำ 200 เมตร<br>
+                                กระจกหน้าปัด Hardlex crystal<br>
+                                สำรองพลังงานได้นานสูงสุดถึง 6 เดือน
+                        </p>
+                        </tr>
+                    </div>
+                </v-card-text>
+              </v-card>
+          </v-col> 
+        </v-row>
+      
+    </v-row>
+    </v-container>
+</template>
